1055c938d6494e215012921261866eaddd982dc9
bummer6799 <108254959+bummer6799@users.noreply.github.com>
2022-10-18 23:08:25 +0800

    fixed some bugs and added b graph

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ screen = pygame.display.set_mode([x, y])
 
 running = True
 
+global memA, memB, memC
+
+memA = 0
+memB = 0
+memC = 0
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,7 +25,7 @@ while running:
 
     screen.fill((255, 255, 255))
 
-    time.sleep(0.5)
+    time.sleep(0.025)
     randInt = np.random.randint(low=1, high=10, size=(1))
     sourceList = ['A', 'B', 'C']
     source = random.randint(0, 2)
@@ -29,8 +35,8 @@ while running:
     print(mainsource)
 
     if sourceList[source] == 'A':
-         global memA
-         memA = intrandint
+        #global memA
+        memA = intrandint
 
     if sourceList[source] == 'A':
         yA = ((y - 40) - (intrandint * ((y - 40) / 10)))
@@ -41,8 +47,17 @@ while running:
 
     pygame.display.flip()
 
+    if sourceList[source] == 'B':
+        #global memB
+        memB = intrandint
 
+    if sourceList[source] == 'B':
+        yB = ((y - 40) - (intrandint * ((y - 40) / 10)))
+        pygame.draw.line(screen, (0, 0, 0), (80, y - 40), (80, yB), width=8)
+    else:
+        yB = ((y - 40) - (memB * ((y - 40) / 10)))
+        pygame.draw.line(screen, (0, 0, 0), (80, y - 40), (80, yB), width=8)
 
+    pygame.display.flip()
 
 pygame.quit()
-
